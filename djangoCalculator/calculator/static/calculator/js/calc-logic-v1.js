872aa2c477b01e7975ab28a9d1ca5fc52f913782
unknown <parthmish@gmail.com>
2020-10-14 15:17:23 +0530

    final commit

--- djangoCalculator/calculator/static/calculator/js/calc-logic-v1.js
+++ djangoCalculator/calculator/static/calculator/js/calc-logic-v1.js
@@ -1,3 +1,46 @@
 $(document).ready(function () {
-    console.log("JS file running")
+    $(".btn").click(function () {
+        if (!$(this).hasClass("not")) {
+            if ($("#expression").val() == 0)
+                $("#expression").val($(this).text());
+            else
+                $("#expression").val($(
+                    "#expression").val() + $(this).text());
+        }
+    });
+
+    $('#clear').click(function () {
+        var value = $("#expression").val();
+        if (!(parseInt(parseFloat(value)) == 0 && value.length == 1))
+            $("#expression").val(value.slice(0, value.length - 1));
+        if (value.length == 1)
+            $("#expression").val("0");
+    });
+
+});
+
+$("#allClear").click(function () {
+    $("#expression").val("0");
+    $("#result").val("0");
+});
+
+$("#equals").click(function () {
+    var result;
+    try {
+        result = (eval(($("#expression").val())));
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            alert("Error! Resetting values.");
+            $("#expression").val("0");
+            $("#result").val("0");
+        }
+        if (e instanceof TypeError) {
+            alert("Error! Resetting values.");
+            $("#expression").val("0");
+            $("#result").val("0");
+        }
+    }
+
+    $("#result").val(result);
+    $("#expression").val("0");
 });
